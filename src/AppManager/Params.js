83fcb429d529474e8a791f8c7831e943c5ac79c7
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-03 23:39:15 +0300

    Update Params.js

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = 'bd5cb87e-bacb-4fd8-b60b-c97e64c5b672';
 export const appID = 'id6743853786';
 export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
-export const targetDate = '2025-04-02';
+export const targetDate = '2025-04-06';
 export const bodyLin = 'https://dazzling-ceremonial-cheer.space/Zm9M3P3d';
